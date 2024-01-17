0bd8b98e95733eed5854286dcc6da4801e027608
Vicente Neto <vneto500@gmail.com>
2024-01-17 16:56:49 -0300

    Atualização na Navbar e pagina de manutencao temporaria - Manutenção nas pastas de arquivos

--- README.md
+++
@@ -1,2 +0,0 @@
-# landingpage-vicente
-- Landing page criada temporariamente para treinar os principios do html, css e js
---
+++ assets/Script/script-git.js
@@ -0,0 +1,2 @@
+// Esse script traz as informações colocandas no github de maneira automatica usado a API do proprio sistema.
+
